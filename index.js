a50aab14000ecc980d1756b524c51264d88f03e3
Ganesh Mallya <ganeshm@BLR.ALLEGISINDIA.COM>
2018-10-15 14:13:41 +0530

    second commit

--- index.js
+++ index.js
@@ -1,22 +1,53 @@
 var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
+var _ = require('lodash');
+var express = require('express');
+var socketUsers = {};
+
+
+var onlineUsers =[];
 
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/Client/index.html');
 });
 
-http.listen(8000, function(){
-  console.log('listening on *:3000');
+http.listen(80, function(){
+  console.log('listening on *:80');
 });
 
+
+
+
+
 io.on('connection', function(socket){
-    console.log('a user connected');
+    let ldapID = socket.handshake.query.name;
+    socket.userLdapID = ldapID;
+    //console.log(socket.handshake);
+    //console.log(socket);
+    //console.log('a user connected : '+ ldapID);
+    onlineUsers.push(ldapID);
+    socketUsers[ldapID] = socket;
+
+    console.log("Online Users :: ");
+    console.log(onlineUsers)
+    
     socket.on('disconnect', function(){
-        console.log('user disconnected');
+        console.log(socket.userLdapID + ' disconnected');
+        _.remove(onlineUsers, function(n) {
+            if(n){
+                return n.toLowerCase() == socket.userLdapID.toLowerCase();
+            }
+            
+          });
+        console.log("Online Users :: ");
+        console.log(onlineUsers)
       });
       socket.on('chat message', function(msg){
-        console.log('message: ' + msg);
-        io.emit('chat message', msg);
+        console.log('message by '+socket.userLdapID+': ' + msg);
+        console.log(socketUsers[socket.userLdapID]);
+        //io.emit('chat message', msg,socket.userLdapID);
+        socketUsers[socket.userLdapID].emit('chat message' , msg,"Bot");
+
       });
   });
